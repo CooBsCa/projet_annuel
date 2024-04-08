35c31f320c6deb4624bffc4432850a603987111d
Lastoss <73163448+CooBsCa@users.noreply.github.com>
2024-04-08 10:35:23 +0200

    - Fix : Db Migrations

---
+++ migration/src/m20220101_000002_alter_table.rs
@@ -0,0 +1,37 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(AppUser::Table)
+                    .add_column(ColumnDef::new(AppUser::Password).string().not_null())
+                    .add_column(ColumnDef::new(AppUser::Password_Len).integer().not_null())
+                    .add_column(ColumnDef::new(AppUser::Salt).string().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(AppUser::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum AppUser {
+    Table,
+    Id,
+    Username,
+    Email,
+    Password,
+    Password_Len,
+    Salt,
+}
